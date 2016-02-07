65d59af21dedae173702499d3f830155b082ce43
Gilberto Villa <ingilniero@gmail.com>
2016-02-07 00:33:54 -0600

    Use babel-loader and the es2015 preset in the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,9 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
+          presets: ['es2015'],
           compact: false
         }
       }
